649f3a4333c5c0a4a71e96a02b16c409d365842d
ashwinkasoju <ashwinkasoju@kas-mbp-2.hub>
2024-10-28 18:10:21 +1300

    test push

--- WhoWantsToBeMillionaire/src/whowantstobemillionaire/WhoWantsToBeMillionaire.java
+++ WhoWantsToBeMillionaire/src/whowantstobemillionaire/WhoWantsToBeMillionaire.java
@@ -14,7 +14,7 @@ public class WhoWantsToBeMillionaire {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       new MainMenuScreen();// starting by launching main screen
     }
     
 }
